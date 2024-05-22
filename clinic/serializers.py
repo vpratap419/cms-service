0631cd5580f272af4ab78b5bae56dd9c86f64a4a
vpratap419 <vpratap419@gmail.com>
2024-05-22 19:51:29 +0530

    Added get,update,delete

--- clinic/serializers.py
+++ clinic/serializers.py
@@ -7,7 +7,7 @@ from clinic.apps import INFO
 
 class CreateClinicSerializer(serializers.ModelSerializer):
 
-    # You have more control before writing the data to DB using DRF. You have add or update custom values
+    # You have more control before writing the data to DB using DRF. You have added or update custom values
     def create(self, data):
         INFO("[%s]CreateClinicSerializer: Data [%s]", data)
         return super(CreateClinicSerializer, self).create(data)
@@ -16,3 +16,23 @@ class CreateClinicSerializer(serializers.ModelSerializer):
         model = Clinic
         fields = ['cid', 'name', 'contact', 'address', 'geolocation', 'website']
 
+
+class ClinicListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clinic
+        fields = ['cid', 'name', 'contact', 'address', 'geolocation', 'website']
+
+
+class UpdateClinicSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return super(UpdateClinicSerializer, self).create(validated_data)
+
+    class Meta:
+        model = Clinic
+        fields = ['cid', 'name', 'contact', 'address', 'geolocation', 'website']
+
+
+class DeleteClinicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clinic
+        fields = ['name', 'contact', 'address', 'geolocation', 'website']
